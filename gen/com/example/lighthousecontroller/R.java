4f15304d36cef4b72493e1eb0f39b1340711ce6d
noface <souzabrizolara@gmail.com>
2014-09-09 11:51:03 -0300

    Protótipo inicial da tela de detalhes da lâmpada.

--- gen/com/example/lighthousecontroller/R.java
+++ gen/com/example/lighthousecontroller/R.java
@@ -1077,9 +1077,12 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int ic_lamp_off=0x7f020057;
+        public static final int ic_lamp_on=0x7f020058;
+        public static final int ic_launcher=0x7f020059;
     }
     public static final class id {
+        public static final int XYPlot=0x7f05003e;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,7 +1094,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
