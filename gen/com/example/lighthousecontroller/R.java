d45e0ab91cd8b2edf5c23f07420f1ff55b458534
noface <souzabrizolara@gmail.com>
2014-09-09 15:04:17 -0300

    Refatorada tela de detalhes de lâmpada

--- gen/com/example/lighthousecontroller/R.java
+++ gen/com/example/lighthousecontroller/R.java
@@ -1245,9 +1245,13 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int bright_label=0x7f0a0011;
+        public static final int lampconsumption_graph_label=0x7f0a0013;
+        public static final int lampconsumption_label=0x7f0a0012;
+        public static final int loremipsum=0x7f0a000f;
+        public static final int power_label=0x7f0a0010;
     }
     public static final class style {
         /** 
